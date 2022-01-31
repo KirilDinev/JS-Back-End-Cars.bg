26e36c7927ccdd7b608b27900d4bc35bf3a80465
Kiki <kirild1997@gmail.com>
2022-01-31 14:41:20 +0200

    implemented car service, home, details and config

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+
+const carService = require('./services/cars.js')
+
+const { about } = require('./controllers/about.js');
+const { create } = require('./controllers/create.js');
+const { details } = require('./controllers/details.js');
+const { home } = require('./controllers/home.js');
+const { notFound } = require('./controllers/notFound.js');
+
+const app = express();
+
+app.engine('hbs', hbs.create({
+    extname: 'hbs',
+}).engine);
+app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('static'));
+app.use(carService());
+
+app.get('/', home);
+app.get('/about', about);
+app.get('/create', create);
+app.get('/details/:id', details);
+
+app.all('*', notFound)
+
+app.listen(3000, () => {
+    console.log("Server strated on port 3000");
+})
