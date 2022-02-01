7b0d1e038155a9ed74753b9c9296c8f21e663156
Kiki <kirild1997@gmail.com>
2022-02-01 18:21:55 +0200

    implemneted edit and delete

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const create = require('./controllers/create.js');
 const { details } = require('./controllers/details.js');
 const { home } = require('./controllers/home.js');
 const { notFound } = require('./controllers/notFound.js');
+const deleteCar = require('./controllers/delete.js');
+const edit = require('./controllers/edit.js');
 
 const app = express();
 
@@ -23,7 +25,10 @@ app.use(carService());
 app.get('/', home);
 app.get('/about', about);
 app.get('/details/:id', details);
-app.route('/create').get(create.get).post(create.post)
+
+app.route('/create').get(create.get).post(create.post);
+app.route('/delete/:id').get(deleteCar.get).post(deleteCar.post);
+app.route('/edit/:id').get(edit.get).post(edit.post)
 
 app.all('*', notFound)
 
--- services/cars.js
+++ services/cars.js
@@ -69,6 +69,28 @@ async function createCar(car) {
     await write(cars)
 }
 
+async function deleteById(id) {
+    const data = await read();
+
+    if (data.hasOwnProperty(id)) {
+        delete data[id]
+        await write(data)
+    } else {
+        throw new ReferenceError('Np such ID in database.');
+    }
+}
+
+async function updateById(id, car) {
+    const data = await read();
+
+    if (data.hasOwnProperty(id)) {
+        data[id] = car
+        await write(data)
+    } else {
+        throw new ReferenceError('Np such ID in database.');
+    }
+}
+
 function nextId() {
     return 'xxxxxxxx-xxxx'.replace(/x/g, () => (Math.random() * 16 | 0).toString(16));
 }
@@ -78,7 +100,8 @@ module.exports = () => (req, res, next) => {
         getAll,
         getById,
         createCar,
-
+        deleteById,
+        updateById,
     };
     next();
 }
